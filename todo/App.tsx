647b7ada860e163fe3a408430bbc64bced9d8d0b
AuntL1ke <semenkos2005@gmail.com.comgit config --global user.email semenkos2005@gmail.com>
2025-03-10 20:24:31 +0000

    home_3

---
+++ todo/App.tsx
@@ -0,0 +1,97 @@
+import { StatusBar } from "expo-status-bar";
+import React, { useEffect, useState } from "react";
+import { Button, FlatList, Pressable, SafeAreaView, ScrollView, StyleSheet, Text, View } from "react-native";
+import TaskCard from "./components/TaskCard";
+
+import axios from "axios";
+import CreateTask from "./components/CreateTask";
+
+
+export type Task = {
+  todo: string,
+  completed: boolean,
+ 
+}
+
+export default function App() {
+  const [tasks, setTasks] = useState<Task[]>([])
+  useEffect(()=>{
+    axios
+      .get("https://dummyjson.com/todos")
+      .then((response)=>{
+    
+        setTasks((prevTasks)=>[...prevTasks,...response.data.todos])
+        console.log(tasks)
+   
+      })
+      .catch((error)=>{
+        console.error("Error fetching tasks:",error)
+      })
+  },[])
+  const [isModalVisible, setIsModalVisible] = useState<boolean>(false)
+  const addTask = (task:Task)=>{
+    setTasks([...tasks,task])
+  }
+  return (
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.title}>TODO List</Text>
+      <Text style={styles.subtitle}>6th March 2025</Text>
+
+      <FlatList
+        style={styles.scrollView}
+        data={tasks}
+        renderItem={({ item }) => <TaskCard task={item} />}
+        keyExtractor={(item, index) => index.toString()}
+      />
+
+      <Pressable style={styles.btn} onPress={()=>setIsModalVisible(true)}>
+        <Text style={styles.btnTitle}>+</Text>
+      </Pressable>
+      <CreateTask isVisible={isModalVisible} onClose={() => setIsModalVisible(false)} onAddTask={addTask} />
+    </SafeAreaView>
+  );
+}
+
+
+
+const styles = StyleSheet.create({
+  container: {
+      flex: 1,
+      alignItems: "center",
+      marginTop: 50,
+      padding: 20,
+  },
+
+  title: {
+      textAlign: "center",
+      fontSize: 42,
+      fontWeight: 700,
+  },
+
+  subtitle: {
+      fontSize: 24,
+      marginTop: 40,
+      marginBottom: 40,
+  },
+
+  scrollView: {
+      width: "100%",
+      marginTop: 20,
+      marginBottom: 20,
+  },
+
+  btn: {
+      width: 60,
+      height: 60,
+      marginTop: -50,
+      borderRadius: "50%",
+      backgroundColor: "#3255f0",
+  },
+
+  btnTitle: {
+      fontSize: 40,
+      textAlign: "center",
+      verticalAlign: "middle",
+      color: "white",
+  },
+});
